f99a8b2de3956c8f4864e29dce7dd91c58c1d47c
heavyn1 <bunedusupov968@gmail.com>
2024-05-04 15:02:10 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+const arraName = [1, 2, 3 = "to'rt" ];
+let l = arraName.length;
+let t 
+alert(l);
+let d = confirm("Siz arraydan element o'chirmoqchimissiz?");
+if (d) {
+    t = arraName.pop()
+}
+alert(t);
+alert(arraName)
+
